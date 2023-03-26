d4dc529361ce9ef74ed75ae5e1d5194e438f3e52
EC2 Default User <ec2-user@ip-172-31-6-120.us-east-2.compute.internal>
2023-03-26 12:24:50 +0000

    no truck images add/fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,11 @@
 # encoding: utf-8
 class ImageUploader < CarrierWave::Uploader::Base  
- storage :fog
+ 
+  if Rails.env.production?
+     include Cloudinary::CarrierWave
+  else
+     storage :file
+  end
 
  # リサイズしたり画像形式を変更するのに必要
   include CarrierWave::RMagick
